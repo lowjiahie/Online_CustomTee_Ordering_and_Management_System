703b8cacbb6e39f8b490e5d4950de8cc87d8a662
Sam Pang <sampang1003@gmail.com>
2022-12-12 02:19:50 +0800

    pull

--- resources/views/admin/orderDetail.blade.php
+++ resources/views/admin/orderDetail.blade.php
@@ -126,7 +126,7 @@
                 <br />
 
                 @foreach ($publishedOrder as $published)
-                    @if ($published->orderItemable_type == 'publishedDesigns')
+                    @if ($published->orderItemable_type == ' App\Models\PublishedDesign')
                         <div class="shadow p-3 mb-5 bg-white rounded" style="width:80%; margin: 0 auto;">
                             <div class='row'
                                 style='background-color: white; color:rgb(0, 0, 0); margin: 10px 10px 10px 10px; padding: 20px 10px 20px 10px;'>
@@ -189,7 +189,7 @@
                 @endforeach
 
                 @foreach ($customTeeOrder as $customTee)
-                    @if ($customTee->orderItemable_type == 'customTees')
+                    @if ($customTee->orderItemable_type == 'App\Models\OrderedCustomTee')
                         <div class="shadow p-3 mb-5 bg-white rounded" style="width:80%; margin: 0 auto;">
                             <div class='row'
                                 style='background-color: white; color:rgb(0, 0, 0); margin: 10px 10px 10px 10px; padding: 20px 10px 20px 10px;'>
