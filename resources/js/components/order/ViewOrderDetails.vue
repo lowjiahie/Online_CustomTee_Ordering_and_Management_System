c04748a968e4cea281e4901ea90b812ad7d12d82
LowJiaHie <jiahielow8@gmail.com>
2022-12-14 12:31:13 +0800

    update cart management

---
+++ resources/js/components/order/ViewOrderDetails.vue
@@ -0,0 +1,22 @@
+<template></template>
+
+<script>
+export default {
+  data() {
+    return {
+        orderDetails:[],
+    };
+  },
+  created() {
+    axios
+      .get(`/api/getOnePresetDesign/${this.$route.params.id}`)
+      .then((response) => {
+      });
+  },
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style>
+</style>
